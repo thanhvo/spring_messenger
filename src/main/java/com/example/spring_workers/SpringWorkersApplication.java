a67e4c8a5ed61d1898e3426bd62978d3e9d00173
thanhvo <thanhvv@gmail.com>
2020-11-25 07:16:57 +0700

    First basic setup.

---
+++ src/main/java/com/example/spring_workers/SpringWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_workers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorkersApplication.class, args);
+	}
+
+}
