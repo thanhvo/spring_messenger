86d47e229817d23ca51746a69f4f98875c73ed49
thanhvo <thanhvv@gmail.com>
2020-11-25 14:12:27 +0700

    A working Spring messenger.

--- src/main/java/com/gfgtech/sc/spring_workers/controller/SqsController.java
+++ src/main/java/com/gfgtech/sc/spring_workers/controller/SqsController.java
@@ -32,7 +32,8 @@ public class SqsController {
     @PostMapping(value = "/send")
     // @ResponseStatus annotation marks the method with the status-code and the reason message that should be returned.
     @ResponseStatus(code = HttpStatus.CREATED)
-    public void sendMessageToSqs(@RequestBody @Valid final SqsMessage message) {
+    public void sendMessageToSqs(@RequestBody @Valid final SqsMessage message)
+    {
         LOGGER.info("Sending the message to the Amazon sqs.");
         queueMessagingTemplate.convertAndSend(QUEUE, message);
         LOGGER.info("Message sent successfully to the Amazon sqs.");
@@ -41,8 +42,9 @@ public class SqsController {
     // @SqsListener listens to the message from the specified queue.
     // Here in this example we are printing the message on the console and the message will be deleted from the queue once it is successfully delivered.
     @SqsListener(value = QUEUE, deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void getMessageFromSqs(@Valid SqsMessage message, @Header("MessageId") String messageId) {
+    public void getMessageFromSqs(@Valid SqsMessage message, @Header("MessageId") String messageId)
+    {
         LOGGER.info("Received message= {} with messageId= {}", message, messageId);
-        // TODO - Developer can do some operations like saving the message to the database, calling any 3rd party etc.
+        LOGGER.info("The content of the message: id = {}, message = {}", message.getId(), message.getMessage());
     }
 }
--- src/main/java/com/gfgtech/sc/spring_workers/domain/SqsMessage.java
+++ src/main/java/com/gfgtech/sc/spring_workers/domain/SqsMessage.java
@@ -4,8 +4,35 @@ public class SqsMessage {
     private String id;
     private String message;
 
-    public SqsMessage(String id, String message) {
+    public SqsMessage()
+    {
+        this.id = null;
+        this.message = null;
+    }
+
+    public SqsMessage(String id, String message)
+    {
         this.id = id;
+        this.message = message;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
         this.message = message;
     }
 }
