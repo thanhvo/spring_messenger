faccfe888891000c50cf94ad25964a386c0997a9
thanhvo <thanhvv@gmail.com>
2020-11-27 15:29:49 +0700

    The reactive listener now continously listens to AWS SQS queue.

--- src/main/java/com/gfgtech/sc/spring_workers/listener/ReactiveSqsListener.java
+++ src/main/java/com/gfgtech/sc/spring_workers/listener/ReactiveSqsListener.java
@@ -1,20 +1,16 @@
 package com.gfgtech.sc.spring_workers.listener;
 
-import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
 import com.amazonaws.services.sqs.model.*;
-import com.gfgtech.sc.spring_workers.domain.SqsMessage;
+import com.gfgtech.sc.spring_workers.subscriber.SqsSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 @Component
 public class ReactiveSqsListener {
@@ -33,29 +29,43 @@ public class ReactiveSqsListener {
     }
 
     @PostConstruct
-    public void continuousListener() {
-        CompletableFuture<ReceiveMessageResult> result = CompletableFuture.supplyAsync(() -> {
-            ReceiveMessageResult message = null;
-            try {
-                message = sqsAsyncClient.receiveMessageAsync(queueUrl).get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-            return message;
-        });
-        Mono<ReceiveMessageResult> receiveMessageResponseMono = Mono.fromFuture(result);
-        receiveMessageResponseMono
-                .repeat()
-                .retry()
-                .subscribe(messages -> {
-                    if (!messages.getMessages().isEmpty()) {
-                        for (Message message : messages.getMessages()) {
-                            LOGGER.info("message body: " + message.getBody());
-                            sqsAsyncClient.deleteMessageAsync(queueUrl, message.getReceiptHandle());
+    public void continuousListener()  {
+        Thread thread = new Thread(){
+            public void run(){
+                while (true) {
+                    CompletableFuture<ReceiveMessageResult> result = CompletableFuture.supplyAsync(() -> {
+                        ReceiveMessageResult message = null;
+                        try {
+                            ReceiveMessageRequest request = new ReceiveMessageRequest()
+                                .withQueueUrl(queueUrl)
+                                .withWaitTimeSeconds(10)
+                                .withVisibilityTimeout(30)
+                                .withMaxNumberOfMessages(10);
+                                message = sqsAsyncClient.receiveMessageAsync(request).get();
+                        } catch (InterruptedException e) {
+                                e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
                         }
-                    }
-                });
+                        return message;
+                    });
+                    Mono<ReceiveMessageResult> receiveMessageResponseMono = Mono.fromFuture(result);
+                    receiveMessageResponseMono
+                            .map(ReceiveMessageResult::getMessages)
+                            .subscribe(messages -> {
+                                        for (Message message : messages) {
+                                            LOGGER.info("message body: " + message.getBody());
+                                            DeleteMessageRequest request = new DeleteMessageRequest()
+                                                    .withQueueUrl(queueUrl)
+                                                    .withReceiptHandle(message.getReceiptHandle());
+                                            sqsAsyncClient.deleteMessageAsync(request);
+                                            LOGGER.info("deleted message with handle " + message.getReceiptHandle());
+                                        }
+                                    }
+                            );
+                }
+            }
+        };
+        thread.start();
     }
 }
---
+++ src/main/java/com/gfgtech/sc/spring_workers/subscriber/SqsSubscriber.java
@@ -0,0 +1,61 @@
+package com.gfgtech.sc.spring_workers.subscriber;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import com.gfgtech.sc.spring_workers.listener.ReactiveSqsListener;
+import org.reactivestreams.Subscription;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.BaseSubscriber;
+
+import java.util.List;
+
+@Component
+public class SqsSubscriber<T> extends BaseSubscriber<T> {
+    public static final Logger LOGGER = LoggerFactory.getLogger(SqsSubscriber.class);
+    private final AmazonSQSAsync sqsAsyncClient;
+    private String queueUrl;
+    private boolean done;
+
+    public SqsSubscriber(AmazonSQSAsync sqsAsyncClient) {
+        this.sqsAsyncClient = sqsAsyncClient;
+        this.done = false;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setQueueUrl(String queueUrl)
+    {
+        this.queueUrl = queueUrl;
+    }
+
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribed");
+        request(1);
+    }
+
+    public void hookOnNext(T item) {
+        ReceiveMessageResult result = (ReceiveMessageResult)item;
+        for (Message message : result.getMessages()) {
+            LOGGER.info("message body: " + message.getBody());
+            DeleteMessageRequest request = new DeleteMessageRequest()
+                    .withQueueUrl(queueUrl)
+                    .withReceiptHandle(message.getReceiptHandle());
+            sqsAsyncClient.deleteMessageAsync(request);
+            LOGGER.info("deleted message with handle " + message.getReceiptHandle());
+        }
+        request(1);
+    }
+
+    public void hookOnComplete() {
+        //request(1);
+        done = true;
+        requestUnbounded();
+    }
+}
