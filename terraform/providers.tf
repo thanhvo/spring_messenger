c2841f1383ccf7ffc65048d5645a8e96e7f9674a
Thanh Vo <thanhvv@gmail.com>
2021-03-22 14:40:17 +0700

    Remove AWS credential data.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,9 +2,4 @@ provider "aws" {
   region  = "eu-central-1"
   alias   = "sc-dev"
   profile = "default"
-  access_key = "AKIAZUXNQEWMJMZZ3HPO"
-  secret_key = "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"
-  assume_role {
-    role_arn = "arn:aws:iam::476125643079:role/OrganizationAccountAccessRole"
-  }
 }
